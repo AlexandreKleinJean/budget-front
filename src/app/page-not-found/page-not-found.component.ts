ef0fb33e42506e4a0b34c0d100c6f21d82a8691c
Alexandre Klein <alexandreklein@macbook-air-de-alexandre.home>
2023-12-28 16:04:55 +0100

    not found page ok

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  templateUrl: './page-not-found.component.html',
+  imports: [CommonModule]
+})
+
+export class PageNotFoundComponent {
+}
