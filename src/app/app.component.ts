6da10db02272050dc14920356c6dd15607fef74f
Alexandre Klein <alexandreklein@AirdeAlexandre.lan>
2024-02-21 12:08:05 +0100

    fontsize responsive on progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './navBar/navBar.component';
 import { NotificationComponent } from './notification/notification.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, NavBarComponent, NotificationComponent]
+  imports: [RouterOutlet, CommonModule, RouterLink, NavBarComponent, NotificationComponent, FooterComponent]
 })
 
 export class AppComponent {
